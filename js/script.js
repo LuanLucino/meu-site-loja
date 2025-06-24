b9648a66c12a4e5e0d2dae15586f316c88d0de0f
Luan Lucino <109971413+LuanLucino@users.noreply.github.com>
2025-06-24 11:24:42 -0300

    comit
    adicionado funções de loja funcional, adicionar/remover itens

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
 // Adicionar item
-function adicionarCarrinho(produto) {
+function adicionarCarrinho(produto, botao) {
+  const span = botao.previousElementSibling.querySelector(".quantidade");
+let quantidade = parseInt(span.textContent);
+
+
   let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-  carrinho.push(produto);
+  const index = carrinho.findIndex(item => item.nome === produto);
+
+  if (index !== -1) {
+    carrinho[index].quantidade += quantidade;
+  } else {
+    carrinho.push({ nome: produto, quantidade });
+  }
+
   localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  alert(`${produto} adicionado ao carrinho!`);
+  atualizarContadorCarrinho();
+  alert(`${quantidade}x ${produto} adicionado(s) ao carrinho!`);
 }
+
+//alterar quantidade 
+
+function alterarQuantidade(botao, delta) {
+  const span = botao.parentElement.querySelector(".quantidade");
+  let valor = parseInt(span.textContent);
+  valor = isNaN(valor) ? 1 : valor + delta;
+  if (valor < 1) valor = 1;
+  span.textContent = valor;
+}
+
 
 // Exibir no carrinho
 if (window.location.pathname.includes("carrinho.html")) {
@@ -19,13 +42,15 @@ if (window.location.pathname.includes("carrinho.html")) {
     let soma = 0;
     container.innerHTML = "";
     carrinho.forEach((item, index) => {
-      soma += 99.9; // valor fictício
-      container.innerHTML += `
-        <div>
-          <p>${item} - R$ 99,90 <button onclick="removerItem(${index})">Remover</button></p>
-        </div>
-      `;
-    });
+  soma += item.quantidade * 99.9; // valor fictício
+  container.innerHTML += `
+    <div>
+      <p>${item.nome} - R$ 99,90 x ${item.quantidade}
+      <button onclick="removerItem(${index})">Remover</button></p>
+    </div>
+  `;
+});
+
     total.textContent = `Total: R$ ${soma.toFixed(2)}`;
   }
 }
@@ -37,3 +62,14 @@ function removerItem(index) {
   localStorage.setItem("carrinho", JSON.stringify(carrinho));
   location.reload();
 }
+
+
+function atualizarContadorCarrinho() {
+  const span = document.getElementById("contador-carrinho");
+  const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  const total = carrinho.reduce((soma, item) => soma + item.quantidade, 0);
+  if (span) span.textContent = `(${total})`;
+}
+
+// Atualiza contador sempre que a página carregar
+atualizarContadorCarrinho();
